5bfa5bba56153e731041d42866e7ca7e2c7a8d7b
WebGCDev <gaetano.chi@gmail.com>
2023-11-21 19:27:25 +0100

    feat: add AC MILAN carousel loading CSS HTML JS Mile1-Mile2-Bonus1

--- js/main.js
+++ js/main.js
@@ -1,44 +1,108 @@
-//Creo array e inserisco immagini in ordine come Template
-const slides = [
-  `img/01.jpg`,
-  `img/05.jpg`,
-  `img/04.jpg`,
-  `img/03.jpg`,
-  `img/02.jpg`,
+const images = [
+  {
+    image: 'img/01.jpg',
+    title: 'Paolo Maldini',
+    text: 'Se da bambino mi fossi scritto una storia, la storia più bella che mi potessi immaginare, avrei scritto il mio sogno come effettivamente mi sta accadendo.',
+  },
+  {
+    image: 'img/02.jpg',
+    title: 'Franco Baresi',
+    text: 'A',
+  },
+  {
+    image: 'img/03.jpg',
+    title: 'Marco Van Basten',
+    text: 'A',
+  },
+  {
+    image: 'img/04.jpg',
+    title: 'Ricardo Kaka',
+    text: 'A',
+  },
+  {
+    image: 'img/05.jpg',
+    title: 'Alessandro Nesta',
+    text: 'A',
+  },
 ];
 
-//Creo costanti
-const btnDown = document.getElementById(`down`);
-const btnUp = document.getElementById(`up`);
-const slideContainer = document.getElementById(`slide-container`);
-let counterSlide = 0;
+//Constanti e Variabili
+const attivaImmagine = document.getElementById('active-img');
+const arrowUp = document.getElementById('arrow-up');
+const arrowDown = document.getElementById('arrow-down');
+const contenitoreThumbnail = document.getElementById('thumbnails');
+let index = 0;
 
-//Creo ciclo for per cambiare le immagini
-for (let i = 0; i < slides.length; i++) {
-  slideContainer.innerHTML += `
-      <img class="slide d-none" src="${slides[i]}">
-    `;
+//Prima immagine dell'array come prima immagine del carousel:
+attivaImmagine.src = images[index].image;
+
+//Titolo e descrizione
+let contenitoreDescrizione = document.getElementById('descrizione');
+
+let contenitoreTitolo = document.createElement('div');
+let contenitoreTesto = document.createElement('div');
+
+contenitoreTitolo.innerHTML = images[index].title;
+contenitoreTesto.innerHTML = images[index].text;
+
+contenitoreTitolo.classList.add('title');
+
+contenitoreDescrizione.append(contenitoreTitolo);
+contenitoreDescrizione.append(contenitoreTesto);
+
+//Creo ciclo for per generare le miniature con l'addEventListener click
+for (let i = 0; i < images.length; i++) {
+  let nuovoThumbnail = document.createElement('img');
+  nuovoThumbnail.src = images[i].image;
+  nuovoThumbnail.style.height = 'calc(100% / ' + images.length + ')';
+  nuovoThumbnail.classList.add('thumbnail');
+  contenitoreThumbnail.append(nuovoThumbnail);
+
+  nuovoThumbnail.addEventListener('click', function () {
+    thumbnailEl[index].classList.remove('active');
+    index = i;
+    attivaImmagine.src = images[i].image;
+    contenitoreTitolo.innerHTML = images[index].title;
+    contenitoreTesto.innerHTML = images[index].text;
+    thumbnailEl[i].classList.add('active');
+  });
 }
 
-const slideSet = document.getElementsByClassName(`slide`);
-slideSet[counterSlide].classList.remove(`d-none`);
+const thumbnailEl = document.querySelectorAll(
+  '.container #thumbnails .thumbnail'
+);
+thumbnailEl[index].classList.add('active');
 
-// BOTTONE UP Attivo la funzione usando addEventListener cosi’ da poter attivare il ciclo infinito
-btnUp.addEventListener(`click`, function () {
-  slideSet[counterSlide].classList.add(`d-none`);
-  counterSlide++;
-  if (counterSlide === slides.length) {
-    counterSlide = 0;
-  }
-  slideSet[counterSlide].classList.remove(`d-none`);
-});
+//Per attivare i bottoni richiamo le funzioni
+arrowDown.addEventListener('click', changeattivaImmagine);
+arrowUp.addEventListener('click', changeattivaImmagineReverse);
 
-// BOTTONE DOWN Attivo la funzione usando addEventListener cosi’ da poter attivare il ciclo infinito
-btnDown.addEventListener(`click`, function () {
-  slideSet[counterSlide].classList.add(`d-none`);
-  if (counterSlide === 0) {
-    counterSlide = slides.length;
+//Funzione tasto in basso
+function changeattivaImmagine() {
+  thumbnailEl[index].classList.remove('active');
+  if (index < images.length - 1) {
+    index++;
+  } else {
+    index = 0;
   }
-  counterSlide--;
-  slideSet[counterSlide].classList.remove(`d-none`);
-});
+
+  attivaImmagine.src = images[index].image;
+  contenitoreTitolo.innerHTML = images[index].title;
+  contenitoreTesto.innerHTML = images[index].text;
+  thumbnailEl[index].classList.add('active');
+}
+
+//Funzione tasto in alto
+function changeattivaImmagineReverse() {
+  thumbnailEl[index].classList.remove('active');
+  if (index > 0) {
+    index--;
+  } else {
+    index = images.length - 1;
+  }
+
+  attivaImmagine.src = images[index].image;
+  contenitoreTitolo.innerHTML = images[index].title;
+  contenitoreTesto.innerHTML = images[index].text;
+  thumbnailEl[index].classList.add('active');
+}
